5dfbdb53373ec5aeb5da9c35997df2424572ce2b
mohandand <mohandand@gmail.com>
2023-06-24 13:59:09 -0500

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,34 @@ import './style.css';
 import Accordion from './Accordion.tsx';
 
 export default function App() {
+  let obj = [
+    {
+      title: 'This is First Accordion',
+      content:
+        'This is Message for Accordion which will colose and open accoding to user actions',
+    },
+    {
+      title: 'This is Second Accordion',
+      content:
+        'This is Message for Accordion which will colose and open accoding to user actions',
+    },
+    {
+      title: 'This is Third Accordion',
+      content:
+        'This is Message for Accordion which will colose and open accoding to user actions',
+    },
+    {
+      title: 'This is Fourth Accordion',
+      content:
+        'This is Message for Accordion which will colose and open accoding to user actions',
+    },
+  ];
   return (
     <div>
       <h2>Accordion</h2>
-      <Accordion
-        title={'This is First Accordion'}
-        content={
-          'This is Message for Accordion which will colose and open accoding to user actions'
-        }
-      />
+      {obj.map((item) => {
+        return <Accordion title={item.title} content={item.content} />;
+      })}
     </div>
   );
 }
